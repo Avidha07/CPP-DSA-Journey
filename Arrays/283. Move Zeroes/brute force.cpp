9efb07d91b5c389bdd056fd97c040a962923de58
Avidha <91987808+Avidha07@users.noreply.github.com>
2026-05-24 01:02:08 +0530

    Create brute force.cpp

---
+++ Arrays/283. Move Zeroes/brute force.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+       vector<int>v(n,0);
+       int ind = 0;
+       for(int i = 0; i<n; i++){
+
+         if(nums[i] != 0)
+         {
+            v[ind] = nums[i];  
+            ind++;
+         }
+       }
+       for(int i =0;i<n; i++)
+       {
+        nums[i] = v[i];
+       }
+        
+    }
+};
