7c69a7eefdcfa99ae63da2966a7744f5ad0c5353
Avidha <91987808+Avidha07@users.noreply.github.com>
2026-06-12 12:44:04 +0530

    Create brute force.cpp

---
+++ Arrays/26. Remove Duplicates from Sorted Array/brute force.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        set<int>st;
+
+        for(int i = 0; i<n; i++)
+        {
+            st.insert(nums[i]);
+        }
+         
+         int i =0;
+        for(auto it : st)
+        {
+            nums[i++] = it;
+        }
+
+        return i;
+    }
+};
