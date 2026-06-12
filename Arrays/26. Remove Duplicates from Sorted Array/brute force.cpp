9da34c0c7b308da55c6b2988e1e21216170a706f
Avidha <91987808+Avidha07@users.noreply.github.com>
2026-06-12 12:51:18 +0530

    Implement removeDuplicates using vector approach
    Added a new implementation of removeDuplicates using a vector to store unique elements.

--- Arrays/26. Remove Duplicates from Sorted Array/brute force.cpp
+++ Arrays/26. Remove Duplicates from Sorted Array/brute force.cpp
@@ -1,3 +1,4 @@
+//using set
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -18,5 +19,33 @@ public:
         }
 
         return i;
+    }
+};
+
+//////////////////////////////////////////////////////////////////
+//using vector
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        vector<int>v;
+        v.push_back(nums[0]);
+
+        for(int i =1; i<n; i++)
+        {
+            if(nums[i] != nums[i-1])
+            {
+               v.push_back(nums[i]);
+            }
+        }
+         int idx = 0;
+        for(auto it : v)
+        {
+             nums[idx++] = it;
+        }
+
+        return idx;
     }
 };
