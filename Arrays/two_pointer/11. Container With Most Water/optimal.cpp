d68a50cc3e48a1b6368efbc89e02ae67007c4d4a
Avidha <91987808+Avidha07@users.noreply.github.com>
2026-06-14 14:41:44 +0530

    Implement optimal solution for Container With Most Water

---
+++ Arrays/two_pointer/11. Container With Most Water/optimal.cpp
@@ -0,0 +1,31 @@
+11. Container With Most Water
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+       int n = height.size();
+       int i = 0;
+       int j =n-1;
+
+       int area = 0;
+       int maxArea = 0;
+
+       while(i<j)
+       {
+         area = min(height[i] , height[j]) * (j-i);
+         maxArea = max(maxArea , area);
+
+         if(height[i] < height[j])
+         {
+            i++;
+         }
+         else{
+            j--;
+         }
+
+       }
+       return maxArea;
+
+    }
+};
