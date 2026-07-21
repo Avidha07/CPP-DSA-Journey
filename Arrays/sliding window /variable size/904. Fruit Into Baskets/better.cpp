40315828d3583eb4b18667bb9b8b5f37b7cc873d
Avidha <91987808+Avidha07@users.noreply.github.com>
2026-07-21 13:07:02 +0530

    Create better.cpp

---
+++ Arrays/sliding window /variable size/904. Fruit Into Baskets/better.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        
+        int maxlen = 0;
+        int n = fruits.size();
+        int i =0; int j =0;
+        unordered_map<int , int>mp;
+
+
+        while(j<n)
+        {
+             mp[fruits[j]]++;
+
+             if(mp.size() > 2)
+             {
+                while(mp.size() > 2)
+                {
+                    mp[fruits[i]]--;
+                    if(mp[fruits[i]] == 0){
+                    mp.erase(fruits[i]);
+                    }
+                    i++;
+                }
+                
+             }
+             if(mp.size()<= 2){
+                maxlen = max(maxlen , j-i+1);
+             }
+             j++;
+        }
+
+        return maxlen;
+
+       
+    }
+};
