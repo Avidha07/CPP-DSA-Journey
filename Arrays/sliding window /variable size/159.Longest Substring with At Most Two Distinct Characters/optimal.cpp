dfb188382fd8072bdf545a245cd3837b9d0c3c1b
Avidha <91987808+Avidha07@users.noreply.github.com>
2026-07-23 16:53:02 +0530

    Create optimal.cpp

---
+++ Arrays/sliding window /variable size/159.Longest Substring with At Most Two Distinct Characters/optimal.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string &s) {
+
+        int n = s.length();
+        int i = 0, j = 0;
+        unordered_map<char, int> mp;
+        int maxLen = 0;
+
+        while (j < n) {
+            mp[s[j]]++;
+
+            while (mp.size() > 2) {
+                mp[s[i]]--;
+                if (mp[s[i]] == 0) {
+                    mp.erase(s[i]);
+                }
+                i++;
+            }
+
+            maxLen = max(maxLen, j - i + 1);
+            j++;
+        }
+
+        return maxLen;
+    }
+};
