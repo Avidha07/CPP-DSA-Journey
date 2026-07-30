3e3f80ea690abf97b9f8aaf065cf4ff52e927353
Avidha <91987808+Avidha07@users.noreply.github.com>
2026-07-30 12:56:53 +0530

    Create optimal.cpp

---
+++ Arrays/sliding window /variable size/209. Minimum Size Subarray Sum/optimal.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int i =0; int j =0;
+        int sum =0; int minl = INT_MAX;
+
+        while(j<n)
+        {
+            sum+=nums[j];
+
+            while(sum >= target)
+            {   
+                minl = min(minl , j-i+1);
+                sum = sum - nums[i];
+                i++;
+            }
+   
+            j++;
+
+        }
+        if(minl == INT_MAX)
+        {
+            return 0;
+        }
+
+        return minl;
+    }
+};
