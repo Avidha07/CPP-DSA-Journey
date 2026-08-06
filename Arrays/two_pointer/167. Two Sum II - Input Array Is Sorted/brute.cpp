f8dd345a9693fcf9febddc57a7d0a8dda64fcd3c
Avidha <91987808+Avidha07@users.noreply.github.com>
2026-08-06 23:14:30 +0530

    Create brute.cpp

---
+++ Arrays/two_pointer/167. Two Sum II - Input Array Is Sorted/brute.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+       
+       int n = numbers.size();
+       for(int i = 0; i<n-1; i++){
+        for(int j = i+1; j<n; j++)
+        {
+            int sum = numbers[i]+numbers[j];
+
+            if(sum == target)
+            {
+                return {i+1 , j+1}; 
+            }
+        }
+
+       }
+       return {-1, -1}; 
+    }
+};
+
+
+//The brute force approach checks every possible pair of elements using two nested loops. For each pair, it calculates their sum and compares it with the target value. If the sum matches the target, it immediately returns the 1-based indices of those two elements. This method is simple and guarantees the correct answer but takes O(n²) time since every pair is examined, while using O(1) extra space.
