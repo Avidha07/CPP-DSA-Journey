d09df6818505f279c82704db9b2c325757303c5d
Avidha <91987808+Avidha07@users.noreply.github.com>
2026-08-06 23:15:02 +0530

    Improve comment formatting in brute.cpp
    Updated comments for better readability.

--- Arrays/two_pointer/167. Two Sum II - Input Array Is Sorted/brute.cpp
+++ Arrays/two_pointer/167. Two Sum II - Input Array Is Sorted/brute.cpp
@@ -20,4 +20,4 @@ public:
 };
 
 
-//The brute force approach checks every possible pair of elements using two nested loops. For each pair, it calculates their sum and compares it with the target value. If the sum matches the target, it immediately returns the 1-based indices of those two elements. This method is simple and guarantees the correct answer but takes O(n²) time since every pair is examined, while using O(1) extra space.
+//The brute force approach checks every possible pair of elements using two nested loops. For each pair, it calculates their sum and //compares it with the target value. If the sum matches the target, it immediately returns the 1-based indices of those two elements. This //method is simple and guarantees the correct answer but takes O(n²) time since every pair is examined, while using O(1) extra space.
